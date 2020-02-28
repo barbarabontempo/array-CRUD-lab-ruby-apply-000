34beaf5465633ead4c51b53b06e4bb675b66a232
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-02-28 23:55:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,10 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  fave_colors["Blue", "Green", "Red", "Yellow"]
-  
+  array["Blue", "Green", "Red", "Yellow"]
+  element = "Orange"
+  array << element
+
 end
 
 def add_element_to_start_of_array(array, element)
