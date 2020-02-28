95393ca768e36d322a0cd8902c939eaf7d9532cc
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-02-28 23:56:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array["Blue", "Green", "Red", "Yellow"]
-  element = "Orange"
+  array[]
   array << element
-
 end
 
 def add_element_to_start_of_array(array, element)
